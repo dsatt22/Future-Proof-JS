16cde35f8d3870da65a60563f78d5237d7114047
Dustin Sattler <dustin.r.sattler@gmail.com>
2020-07-14 14:56:36 +0000

    just added in notes for the different things you can do with JS.

---
+++ main.js
@@ -0,0 +1,42 @@
+//numbers
+// var total = ((2 * 4) * 5 ) / 2;
+// total
+
+//strings 
+// var myName = '"Dustin Sattler"'
+// myName
+
+//boolean
+// var studentPassed = true
+// studentPassed
+
+// if (studentPassed) {
+//   console.log('hey he passed')
+// } else {
+//   console.log('sorry he failed, must take class again')
+// }
+// studentPassed
+
+//Array
+// var userData = ["dustin", 54, 654, 684, "aaron", 'Jared']
+// userData
+
+//Objects
+// var person = {
+//   name: 'Dustin',
+//   age: '30',
+//   kids: false,
+//   certificates: ['CompTIA Security+', 'CompTIA A+'],
+//   printName: function(){
+//     console.log('my name is Dustin')
+//   }
+// }
+// person.printName()
+
+//Undefined
+// var bestTeam;
+// console.log(bestTeam)
+
+//Null
+// var student = null;
+// console.log(student)
