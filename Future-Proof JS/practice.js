16033b4094a89dd3a803495f047448d10177d22b
Dustin Sattler <dustin.r.sattler@gmail.com>
2020-07-15 12:44:02 +0000

    added in a notes.js and practice.js files. Also created a Future-Proof JS folder

---
+++ Future-Proof JS/practice.js
@@ -0,0 +1,37 @@
+//NUMBERS
+
+/*var a = 20 * 5
+console.log(a)
+console.log('======================================')
+
+var charly = (((16-2) * 2) / 4)
+console.log(charly)
+console.log('======================================')
+a * charly*/
+
+
+//STRINGS
+
+/*var myName = 'Dustin Robert Sattler'
+myName.toUpperCase()
+
+var d = 'Dustin'
+console.log(d)
+console.log('================================')
+console.log(d.toUpperCase())
+console.log('================================')
+console.log(d.toLowerCase())
+console.log('================================')
+
+var s = 'Sattler'
+console.log(s)
+console.log('================================')
+
+console.log(d + s)
+console.log('================================')*/
+
+//ARRAYS
+
+var users = ["Matt", "cam", "Barbara", "Bryant", "Joseph"]
+
+console.log(users[0])
